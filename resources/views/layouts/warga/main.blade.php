e82b79aa5ce6bd4576edd523ae8bb0391609d3e2
fahrizal-aulia <fahrizalaulia@gmail.com>
2024-08-11 22:37:05 +0700

    update

--- resources/views/layouts/warga/main.blade.php
+++ resources/views/layouts/warga/main.blade.php
@@ -22,10 +22,7 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-left">
-                        <p>2022 &copy; Voler</p>
-                    </div>
-                    <div class="float-right">
-                        <p>Crafted with <span class="text-danger"><i data-feather="heart"></i></span> by <a href="http://ahmadsaugi.com">Ahmad Saugi</a></p>
+                        <p>Copyright &copy; SIMDABESMIWA 2024</p>
                     </div>
                 </div>
             </footer>
--- resources/views/warga/dashboard.blade.php
+++ resources/views/warga/dashboard.blade.php
@@ -6,7 +6,6 @@
     <div class="row">
         <div class="col-12 col-md-6 order-md-1 order-last">
             <h3>Datatable</h3>
-            <p class="text-subtitle text-muted">We use 'simple-datatables' made by @fiduswriter. You can check the full documentation <a href="https://github.com/fiduswriter/Simple-DataTables/wiki">here</a>.</p>
         </div>
     </div>
 </div>
